fe0d384949ff17e93ce296df6d95bb4ed4806e30
manu olivar <jolivar@tse-org.gt>
2021-03-04 12:16:27 -0600

    mi op added

---
+++ src/main/java/com/example/springsocial/controller/DatosOrganizacionesPoliticasController.java
@@ -0,0 +1,77 @@
+package com.example.springsocial.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.springsocial.crud.ObjectSetGet;
+import com.example.springsocial.error.CustomException;
+import com.example.springsocial.error.ErrorCode;
+import com.example.springsocial.model.OrganizacionesPoliticas;
+import com.example.springsocial.repository.OrganizacionesPoliticasRepository;
+import com.example.springsocial.repository.PadronElectoralRepository;
+import com.example.springsocial.security.CurrentUser;
+import com.example.springsocial.security.UserPrincipal;
+import com.example.springsocial.tools.RestResponse;
+
+@SuppressWarnings({"rawtypes","unchecked"})
+@RestController
+@RequestMapping("organizacionesPoliticas")
+public class DatosOrganizacionesPoliticasController {
+	@Autowired
+	private OrganizacionesPoliticasRepository repository;	  	
+	
+	@GetMapping("list/{cui}")
+    public RestResponse openFile(@CurrentUser UserPrincipal userPrincipal, 
+    		HttpServletRequest request,
+    		@PathVariable String cui) throws Exception {
+		ObjectSetGet data= new ObjectSetGet();
+		JSONObject jsonResponse = new JSONObject();
+		String nroBoleta, splitData[];
+		RestResponse response = new RestResponse();
+		try {
+			List list= repository.listUserData(cui);
+			
+			if (!list.isEmpty()) {
+				splitData = list.get(0).toString().split(",");	
+				jsonResponse.put("name", splitData[0]);
+				jsonResponse.put("idOrganizacion", splitData[1]);
+				jsonResponse.put("siglas", splitData[2]);
+				jsonResponse.put("departamento", splitData[3]);
+				jsonResponse.put("municipio", splitData[4]);
+				jsonResponse.put("direccion", splitData[5]);
+				jsonResponse.put("telefono", splitData[6]);
+				jsonResponse.put("email", splitData[7]);
+				jsonResponse.put("representanteLegal", splitData[8]);
+				jsonResponse.put("fase", splitData[9]);
+				jsonResponse.put("estado", splitData[10]);
+
+				String siglas = splitData[2].trim();
+				
+				System.out.println(siglas);
+				
+
+			}else {
+				return new RestResponse(null,new CustomException("CUI NO ASOCIADO A NINGUNA ORGANIZACIÓN POLITICA"
+						+ "",ErrorCode.REST_CREATE,this.getClass().getSimpleName(),0));
+
+			}
+			
+		
+			response.setData(jsonResponse);
+		}catch(Exception exception) {
+			CustomException customExcepction=  new CustomException(exception.getMessage(),exception,ErrorCode.REST_UPDATE,this.getClass().getSimpleName());
+			response.setError(customExcepction);
+		}
+		
+    	return response;
+    }
+	
+}
---
+++ src/main/java/com/example/springsocial/repository/OrganizacionesPoliticasRepository.java
@@ -0,0 +1,38 @@
+package com.example.springsocial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.springsocial.model.Company;
+import com.example.springsocial.model.OrganizacionesPoliticas;
+
+@Repository
+@Transactional
+public interface OrganizacionesPoliticasRepository extends JpaRepository<OrganizacionesPoliticas, String> 
+{   
+
+	@Query(value = "select nombreop as nombre, '' as idop, siglas, d1.deslarga as departamento, d2.deslarga as municipio," 
+			+"decode(o.direccion,null,'', o.direccion || decode(o.nrocasa,null,' ',o.nrocasa) || decode(o.nrozona,null,'',' ZONA ' || o.nrozona)) as direccion,"
+			+"telefono, email, p.nom1 || ' ' || decode(p.nom2,null,'',p.nom2 || ' ')  ||p.ape1 || decode(p.ape2,null,'',' ' || p.ape2) || decode(trim(p.ape3),null,'',' DE ' || p.ape3) as REPRESENTANTELEGAL,"
+			+"upper(r1.descripcion) as fase , upper(r2.descripcion) as estado from tpadron p, tdpi d, tafiliados a, torgpoliticas o, trefdepmun d1, trefdepmun d2, trefstatusop r1, trefstatusop r2 "
+			+"where d1.coddep = o.coddep and d1.codmun = 0 "
+			+"and d2.coddep = o.coddep and d2.codmun = o.codmun "
+			+"and p.nroboleta = o.bolreplegal "
+			+"and r1.codigo = o.faseop and r1.objeto = 'FaseOP'"
+			+"and r2.codigo = o.statusop and r2.objeto = 'StatusOP'"
+			+"and o.idop = a.idop "
+			+"and a.nroboleta = d.nroboleta "
+			+"and d.cui = :cui",nativeQuery = true)
+	
+	
+		/*Persona listarDatos(@Param("cui") String cui);*/
+	//List<OrganizacionesPoliticas> listUserData(@Param("cui") String cui);
+List<String> listUserData(@Param("cui") String cui);
+
+
+}
