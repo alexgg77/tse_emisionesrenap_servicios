a0ece12c2e5091356b1850b7af339589695704be
José Galindo <jgalindo@tse.org.gt>
2021-06-03 13:27:57 -0600

    modified consulting

--- src/main/java/com/example/springsocial/repository/OrganizacionesPoliticasRepository.java
+++ src/main/java/com/example/springsocial/repository/OrganizacionesPoliticasRepository.java
@@ -31,14 +31,13 @@ public interface OrganizacionesPoliticasRepository extends JpaRepository<Organiz
 	//Persona listarDatos(@Param("cui") String cui);
 	//List<OrganizacionesPoliticas> listUserData(@Param("cui") String cui);
 	
-	@Query(value="select unique nombreop as name, a.idop as idOrganizacion, siglas as siglas, d1.deslarga as departamento, d2.deslarga as municipio,"
-			+"decode(o.direccion,null,'', o.direccion || decode(o.nrocasa,null,' ',o.nrocasa) || decode(o.nrozona,null,'',' ZONA ' || o.nrozona)) as direccion,"
-			+"telefono, email, p.nom1 || ' ' || decode(p.nom2,null,'',p.nom2 || ' ')  ||p.ape1 || decode(p.ape2,null,'',' ' || p.ape2) "
-			+"|| decode(trim(p.ape3),null,'',' DE ' || p.ape3) as representanteLegal, upper(r1.descripcion) as fase , upper(r2.descripcion)"
-			+" as estado from tpadron p, tdpi d, tafiliados a, torgpoliticas o, trefdepmun d1, trefdepmun d2, trefstatusop r1, trefstatusop r2 "
-			+"where d1.coddep = o.coddep and d1.codmun = 0 and d2.coddep = o.coddep and d2.codmun = o.codmun and p.nroboleta = o.bolreplegal "
-			+"and r1.codigo = o.faseop and r1.objeto = 'FaseOP' and r2.codigo = o.statusop and r2.objeto = 'StatusOP'"
-			+"and o.idop = a.idop and d.nroboleta=a.nroboleta and o.idop=:idop",nativeQuery=true)
+	@Query(value="select nombreop as name, o.idop as idOrganizacion, siglas as siglas, d1.deslarga as departamento, d2.deslarga as municipio, "
+			+ "decode(o.direccion,null,'', o.direccion || decode(o.nrocasa,null,' ',o.nrocasa) || decode(o.nrozona,null,'',' ZONA ' || o.nrozona)) as direccion, "
+			+ "telefono, email, p.nom1 || ' ' || decode(p.nom2,null,'',p.nom2 || ' ')  ||p.ape1 || decode(p.ape2,null,'',' ' || p.ape2) "
+			+ "|| decode(trim(p.ape3),null,'',' DE ' || p.ape3) as representanteLegal, upper(r1.descripcion) as fase , upper(r2.descripcion) "
+			+ "as estado from torgpoliticas o,tpadron p, trefdepmun d1, trefdepmun d2, trefstatusop r1, trefstatusop r2  "
+			+ "where d1.coddep = o.coddep and d1.codmun = 0 and d2.coddep = o.coddep and d2.codmun = o.codmun  and r1.codigo = o.faseop  "
+			+ "and r1.objeto = 'FaseOP' and r2.codigo = o.statusop and r2.objeto = 'StatusOP'and p.nroboleta=o.bolreplegal and o.idop=:idop",nativeQuery=true)
 	List<String> listUserData(@Param("idop") String op);
 
 
